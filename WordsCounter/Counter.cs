e41e728b08408860fd1d73752a1f390fec4790bf
Denis Dobra <82820448+denisdobra10@users.noreply.github.com>
2022-12-24 15:28:31 +0200

    Add files via upload

---
+++ WordsCounter/Counter.cs
@@ -0,0 +1,76 @@
+﻿namespace WordsCounter
+{
+    public class Counter
+    {
+        private string sentence;
+        private int sentenceLength;
+        private int sentenceLengthWithoutWhiteSpaces;
+
+        private string[] wordsList;
+        private int wordsCount;
+
+        private Dictionary<string, int> wordsDictionary;
+
+
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public Counter(string _sentence) => Setup(_sentence);
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+
+        private void Setup(string _sentence)
+        {
+            this.sentence = _sentence;
+            this.sentenceLength = sentence.Length;
+            sentenceLengthWithoutWhiteSpaces = 0;
+            foreach (string word in sentence.Split(' '))
+                sentenceLengthWithoutWhiteSpaces += word.Length;
+
+            this.wordsList = sentence.Split(' ');
+            this.wordsCount = wordsList.Length;
+
+            // Setup wordsDictionary
+            this.wordsDictionary = new Dictionary<string, int>();
+            CalculateWords();
+        }
+
+
+        // Getters
+        public string GetSentence() { return this.sentence; }
+
+        public int GetSentenceLength() { return this.sentenceLength; }
+
+        public string[] GetWordsList() { return this.wordsList; }
+
+        public int GetWordsCount() { return this.wordsCount; }
+
+        public int GetSentenceLengthWithoutWhiteSpaces() { return this.sentenceLengthWithoutWhiteSpaces; }
+
+
+
+        // Private methods
+        private void CalculateWords()
+        {
+            foreach (string word in wordsList)
+            {
+                if (wordsDictionary.ContainsKey(word))
+                {
+                    wordsDictionary[word]++;
+                }
+                else
+                {
+                    wordsDictionary.Add(word, 1);
+                }
+            }
+        }
+
+
+        // Public Methods
+        public Dictionary<string, int> GetWordsDictionary() { return this.wordsDictionary; }
+
+        public void UpdateSentence(string _sentence)
+        {
+            Setup(_sentence);
+        }
+    }
+}
---
+++ WordsCounter/Program.cs
@@ -0,0 +1,47 @@
+﻿using WordsCounter;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int counter = 0;
+
+        while(true)
+        {
+            counter++;
+            RunProgram();
+
+            Console.Write("Do you wish to continue? (y/n): ->");
+            if (Console.ReadLine().ToLower().Contains('n'))
+                break;
+            else
+                Console.WriteLine();
+        }
+
+        Console.Write("\nThanks for using the program.\nYou have used the alogirthm " + counter + " times!");
+
+    }
+
+
+    private static void RunProgram()
+    {
+        Console.Write("Enter sentence: ");
+        Counter wordCounter = new Counter(Console.ReadLine());
+
+        Console.WriteLine("The user given sentence is: " + wordCounter.GetSentence());
+        Console.WriteLine("The sentence has: " + wordCounter.GetSentenceLength() + " characters including whitespaces" +
+            " and " + wordCounter.GetSentenceLengthWithoutWhiteSpaces() + " characters not including the white spaces!");
+        Console.WriteLine("");
+
+        Console.WriteLine("Sentence has " + wordCounter.GetWordsCount() + " words");
+        Console.WriteLine("");
+
+        Console.WriteLine("List of words and number of times the specific words appeared in the sentence: ");
+        foreach (string word in wordCounter.GetWordsDictionary().Keys)
+        {
+            Console.WriteLine("Word '" + word + "' appeared " + wordCounter.GetWordsDictionary()[word] + " times!");
+        }
+    }
+
+
+}
